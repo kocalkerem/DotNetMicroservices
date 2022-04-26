d068339a6c090bf7035545c973ace7c03748f558
keremkocal <keremkocal@neredekal.com>
2022-04-26 23:41:20 +0300

    new actions and docker config files added in the basket project.

--- src/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -9,6 +9,8 @@ using System.Threading.Tasks;
 
 namespace Basket.API.Controllers
 {
+    [ApiController]
+    [Route("api/v1/[controller]")]
     public class BasketController: ControllerBase
     {
         private readonly IBasketRepository _basketRepository;
@@ -18,10 +20,19 @@ namespace Basket.API.Controllers
             _basketRepository = basketRepository ?? throw new ArgumentNullException(nameof(basketRepository));
         }
 
+        [HttpGet("{userName}", Name = "GetBasket")]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
         public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
         {
             var basket = await _basketRepository.GetBasket(userName);
             return Ok(basket ?? new ShoppingCart(userName));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody]ShoppingCart basket)
+        {
+           return Ok( await _basketRepository.UpdateBasket(basket));
         }
 
         [HttpDelete("{userName}", Name = "DeleteBasket")]
